1ed95df42e213bf745587d10fd9b6ae1126ee919
mvLaboratory <mvlabaratory@gmail.com>
2016-11-29 22:25:10 +0200

    eurika & client

--- eureka-service/src/main/java/edu/mykytiuk/EurekaServiceApplication.java
+++ eureka-service/src/main/java/edu/mykytiuk/EurekaServiceApplication.java
@@ -2,13 +2,13 @@ package edu.mykytiuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-public class EasytemperatureApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasytemperatureApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 }
--- temperature-client/src/main/java/edu/mykytiuk/TemperatureClientApplication.java
+++ temperature-client/src/main/java/edu/mykytiuk/TemperatureClientApplication.java
@@ -3,12 +3,14 @@ package edu.mykytiuk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EasytemperatureApplication {
+public class TemperatureClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasytemperatureApplication.class, args);
+		SpringApplication.run(TemperatureClientApplication.class, args);
 	}
 }
